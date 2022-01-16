ee11e3de0569fc2dfe1cfaacf761c08926d32715
AndersonNBK <andersonaugustonb@ufps.edu.co>
2022-01-16 15:25:22 -0500

    Terminando las Cards y la paginacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
+import Pagination from './components/Pagination/Pagination';
 
 function App() {
+//Con esta variables controlamos en que página estamos
+  let [pageNumber, setPageNumber] = useState(1);
+  let[fetchedData, updateFetchedData] = useState([]);
+  let {info,results} = fetchedData
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+
+  useEffect(()=>{
+
+    (async function(){
+      let data = await fetch(api).then(res=>res.json());
+      updateFetchedData(data);
+    })();
+  },[api]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center ubuntu my-4">
+        Rick & Morty <span className="text-primary">Wiki</span>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters></Filters>
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards results={results}></Cards>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Pagination numeroPagina={pageNumber} setPageNumber={setPageNumber}></Pagination>
     </div>
   );
 }
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Pagination = ({ numeroPagina, setPageNumber }) => {
+  let next = () => {
+    setPageNumber((x) => x + 1);
+  };
+
+  let prev = () => {
+    if (numeroPagina === 1) return;
+    setPageNumber((x) => x - 1);
+  };
+
+  return (
+    <div className="container d-flex justify-content-center gap-5 my-5">
+      Pagination
+      <button onClick={prev} className="btn btn-primary">
+        Prev
+      </button>
+      <button onClick={next} className="btn btn-primary">
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
